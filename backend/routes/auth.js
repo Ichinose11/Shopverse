66fda88c51231cb8ed5d1d9a48333f8770a5463c
Ichinose11 <aloka8504@gmail.com>
2026-03-14 22:12:44 +0530

    authentication for users using token jwr

---
+++ backend/routes/auth.js
@@ -0,0 +1,12 @@
+// routes/auth.js
+const express = require('express');
+const router = express.Router();
+const { signup, login, getMe, updateMe } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/me', protect, getMe);
+router.put('/me', protect, updateMe);
+
+module.exports = router;
